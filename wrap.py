68a2fad5bf6fec46eeffeebe190206a0c45eb09f
Joanna <joannaunswit@gmail.com>
2020-09-08 16:20:24 +1000

    init design

---
+++ wrap.py
@@ -0,0 +1,40 @@
+#read log line by line
+def read_log(filename):
+    pass
+
+# decode each line to a string[](split)
+def decode_line():
+    pass
+
+#output as requested to csv file
+def csv_wrap(line):
+    pass
+
+#check each message 10 legnth*256 = checksum
+def checkSum():
+    pass
+
+
+# check message type(35)
+#    8 -- excuate 
+#    0 -- ignore 
+def messageTypeCheck():
+    pass
+
+# time format transfer
+def time_format():
+    pass
+
+# add unit test cases
+def Unit_test():
+    pass
+
+
+#advance
+class Message(object):
+    pass
+
+class Unit_test(object):
+    pass
+
+# GUI
