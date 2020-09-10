615852e178aea340483f61cc681e2e6d872625df
Joanna <joannaunswit@gmail.com>
2020-09-10 21:31:35 +1000

    update unit test bugs

--- unit_test/test_wrap.py
+++ unit_test/test_wrap.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     test_dir = os.path.abspath(os.path.join(path, 'unit_test'))
     discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')
 
-    # runner = unittest.TextTestRunner(verbosity=2)
-    # runner.run(discover)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(discover)
 
-    report_file = '../output/test_report.txt'
-    report_file = os.path.abspath(os.path.join(path, 'output/test_report.txt'))
-    with open(report_file, "w") as report_file:
-        runner = unittest.TextTestRunner(stream=report_file, verbosity=2)
-        runner.run(discover)
+    # report_file = '../output/test_report.txt'
+    # report_file = os.path.abspath(os.path.join(path, 'output/test_report.txt'))
+    # with open(report_file, "w") as report_file:
+    #     runner = unittest.TextTestRunner(stream=report_file, verbosity=2)
+    #     runner.run(discover)
